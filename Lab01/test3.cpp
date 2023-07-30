baf0e56b7ffd416fbf687acfef5a3b81d4473121
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-07-30 15:04:41 +0600

    Add files via upload

---
+++ Lab01/test3.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"Hellow in my university";
+    cout<<endl;
+    cout<<"Im am student in it faculty";
+    cout<<endl;
+    cout<<"Welcome";
+    return 0;
+}
