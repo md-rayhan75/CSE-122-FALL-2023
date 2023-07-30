a9dfcf85f7a91e7b2021719791ef7bc4948f782e
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-07-30 16:27:35 +0600

    Add files via upload

---
+++ test5.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int  main(){
+    int x;
+    cin>>x;
+    if(x>=0){
+        cout<<"x value is positive number"<<endl;
+        if(x%2==0){
+            cout<<"x value is even number "<<endl;
+            if(x>999& x<10000){
+                cout<<"x value contains 4  digits "<<endl;
+            }
+        }
+    }
+    return 0;
+}
