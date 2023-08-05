7e2ec4c588e7f097c0a0263c2c23901761076761
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-08-05 21:07:53 +0600

    Update and rename A.way to long words to A.way to long words.cpp

---
+++ lab2/A.way to long words.cpp
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<string.h>
+   int main()
+  {
+ 
+    int len,k,x;
+    char str[101];
+    scanf("%d",&x);
+    while (x--)
+    {
+        scanf("%s",&str);
+        len =strlen(str);
+        if(len<=10)
+        printf("%s\n",str);
+        else
+        printf("%c%d%c\n",str[0],len-2,str[len-1]);
+    }
+ 
+    return 0;
